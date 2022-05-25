3af67b15a49811b15d6443d46e16f5401ee37e35
BISINIGISHIVANI <bisinigishivani@gmail.com>
2022-05-25 11:31:13 +0530

    refactor: update styles of notecard & sidebar

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,16 +1,27 @@
 import "./sidebar.css";
 import {Link} from "react-router-dom";
 export function Sidebar(){
-    return <>
-    <div className="sidebar-cflex">
-    <Link to="/notes">
-    <label><i class="fa-solid fa-book"></i>Notes</label>
-    </Link>
-    <label><i className="fa-solid fa-tag"></i> Label</label>
-    <label><i className="fa-solid fa-box-archive"></i> Archive</label>
-    <label><i className="fa-solid fa-trash"></i> Trash</label>
-    <label><i className="fa-solid fa-user"></i> Profile</label>
-    </div>
-    <div className="border-right"></div>
-    </>
+    return (
+      <>
+        <div className="sidebar-cflex flex-wrap margin-top">
+          <label>
+            <Link to="/notes">
+              <i class="fa-solid fa-book"></i> Notes
+            </Link>
+          </label>
+          <label>
+            <i className="fa-solid fa-tag"></i> Label
+          </label>
+          <label>
+            <i className="fa-solid fa-box-archive"></i> Archive
+          </label>
+          <label>
+            <i className="fa-solid fa-trash"></i> Trash
+          </label>
+          <label>
+            <i className="fa-solid fa-user"></i> Profile
+          </label>
+        </div>
+      </>
+    );
 }
