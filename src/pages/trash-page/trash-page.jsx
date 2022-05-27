81d32af15656e7162ade7ff6ba49fee2e35e1353
BISINIGISHIVANI <bisinigishivani@gmail.com>
2022-05-27 21:52:34 +0530

    refactor:add trash functionilities

--- src/pages/trash-page/trash-page.jsx
+++ src/pages/trash-page/trash-page.jsx
@@ -1,4 +1,34 @@
 import "./trash-page.css";
+import { NavBar, Sidebar } from "../../components";
+import { useTrash } from "../../hooks/context/trash-context";
+import { TrashNoteCard } from "../../components/notes-component/trashNote-card";
 export default function TrashPage() {
-    return <></>
+  const { trashNote } = useTrash();
+  return (
+    <>
+      <NavBar />
+      <div className="flex-row">
+        <div className="position-right">
+          <Sidebar />
+        </div>
+        <div className="flex-grow">
+          <div className="align-md mg-left-sm">
+            <div className="flex-wrap">
+              {trashNote.length > 0 ? (
+                <div className="note-list">
+                  {trashNote.map((item) => (
+                    <TrashNoteCard key={item._id} {...item} />
+                  ))}
+                </div>
+              ) : (
+                <div className="margin-auto">
+                  <h2 className="padding-md margin-top">No Trash Notes</h2>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
